c238593b5abe4a8ceb39f37e3fe01aefd1b35472
jaykim <esmussseinhj@gmail.com>
2013-12-09 19:45:27 +0900

    added outputs for SR

--- IR.v
+++ IR.v
@@ -14,7 +14,8 @@ module IR (
   output [2:0]  Rs0,
   output [2:0]  Rs1,
   output [2:0]  Rs2,
-  output [2:0]  Rs3
+  output [2:0]  Rs3,
+  output [8:0]  Rl
 );
 
   reg [15:0] inst_reg;
@@ -38,6 +39,7 @@ module IR (
   assign immed7   = inst_reg[6:0];
   assign immed8   = inst_reg[7:0];
   assign immed11  = inst_reg[10:0];
+  assign Rl       = inst_reg[8:0];
   assign Rs0      = inst_reg[2:0];
   assign Rs1      = inst_reg[5:3];
   assign Rs2      = inst_reg[8:6];
